ac611bed0f56cbe67f33c4861522d84edcb761d2
BaturevichNikita <baturevich_nikita@outlook.com>
2020-11-27 15:36:45 +0300

    fix: don't send auth header if empty

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -31,6 +31,8 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
   };
 
   const uploadFile = async (e: any) => {
+      const authToken = localStorage.getItem('authorization_token');
+      const authHeader = authToken ? {'Authorization': `Basic ${authToken}`} : {};
       // Get the presigned URL
       const response = await axios({
         method: 'GET',
@@ -38,9 +40,7 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
         params: {
           name: encodeURIComponent(file.name)
         },
-         headers: {
-          'Authorization': `Basic ${localStorage.getItem('authorization_token')}`
-        }
+        headers: {...authHeader}
       })
       console.log('File to upload: ', file.name)
       console.log('Uploading to: ', response.data)
